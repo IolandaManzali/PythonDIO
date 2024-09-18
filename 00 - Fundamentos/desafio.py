a9d8dc06ceeaf154372ae7f265f1aa09ca49e77f
Gustavo Raia <gustavo.raia02@gmail.com>
2024-09-18 16:50:12 -0300

    Desafio Banco Python

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -18,48 +18,84 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-        valor = float(input("Informe o valor do depósito: "))
+        '''
+        Operação de depósito
+        
+        Deve ser possível depositar valores positivos para a minha
+        conta bancária. A v1 do projeto trabalha apenas com 1
+        usuário, dessa forma não precisamos nos preocupar em
+        identificar qual é o número da agência e conta bancária. Todos
+        os depósitos devem ser armazenados em uma variável e
+        exibidos na operação de extrato.
+        '''
 
-        if valor > 0:
-            saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
+        valor_deposito = float(input("Digite o valor do depósito: "))
+
+        if(valor_deposito > 0):
+            saldo += valor_deposito
+            extrato += f"Depósito: R$ {valor_deposito}\n"
+            print("Depósito realizado com sucesso.")
+            print("Saldo atual: R$", saldo)
 
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            print("Valor inválido")
 
     elif opcao == "s":
-        valor = float(input("Informe o valor do saque: "))
+        '''
+        Operação de saque
 
-        excedeu_saldo = valor > saldo
+        O sistema deve permitir realizar 3 saques diários com limite
+        máximo de R$ 500,00 por saque. Caso o usuário não tenha
+        saldo em conta, o sistema deve exibir uma mensagem
+        informando que não será possível sacar o dinheiro por falta de
+        saldo. Todos os saques devem ser armazenados em uma
+        variável e exibidos na operação de extrato.
+        '''
 
-        excedeu_limite = valor > limite
+        valor_saque = float(input("Digite o valor do saque: "))
 
-        excedeu_saques = numero_saques >= LIMITE_SAQUES
+        if(valor_saque > saldo):
+            print("Saldo insuficiente para realizar o saque.")
 
-        if excedeu_saldo:
-            print("Operação falhou! Você não tem saldo suficiente.")
+        elif(valor_saque > limite):
+            print("Valor excede o limite.")
 
-        elif excedeu_limite:
-            print("Operação falhou! O valor do saque excede o limite.")
-
-        elif excedeu_saques:
-            print("Operação falhou! Número máximo de saques excedido.")
-
-        elif valor > 0:
-            saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
+        elif(numero_saques >= LIMITE_SAQUES):
+            print("Limite de saques diários excedido.")
+        
+        elif(valor_saque > 0):
+            saldo -= valor_saque
+            extrato += f"Saque: R$ {valor_saque}\n"
+            print("Saque realizado com sucesso.")
+            print("Saldo atual: R$", saldo)
             numero_saques += 1
 
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            print("Valor inválido")
 
     elif opcao == "e":
-        print("\n================ EXTRATO ================")
-        print("Não foram realizadas movimentações." if not extrato else extrato)
-        print(f"\nSaldo: R$ {saldo:.2f}")
-        print("==========================================")
+        '''
+        Operação de extrato
+
+        Essa operação deve listar todos os depósitos e saques
+        realizados na conta. No fim da listagem deve ser exibido o
+        saldo atual da conta. Se o extrato estiver em branco, exibir a
+        mensagem: Não foram realizadas movimentações.
+        Os valores devem ser exibidos utilizando o formato R$ xxx.xx,
+        exemplo:
+        1500.45 = R$ 1500.45
+        '''
+
+        print("\n-----------------------------------------")
+        print("\t\tExtrato")
+        print("-----------------------------------------\n")
+        if(extrato == ""):
+            print("Não foram realizadas movimentações.")
+        print("Saldo: R$", saldo)
+        print("\n-----------------------------------------")
 
     elif opcao == "q":
+        print("Saindo...")
         break
 
     else:
